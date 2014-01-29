d370af471bc86c335aab71a85683ff08b9d6df35
yaron samuel <samuel.yaron@gmail.com>
2014-01-29 21:39:56 +0200

    Few changes by yaron 29.1

--- my_comment_app/forms.py
+++ my_comment_app/forms.py
@@ -4,4 +4,5 @@ from django.contrib.comments.forms import CommentForm
 from django.utils.translation import ungettext, ugettext, ugettext_lazy as _
 
 class MyCommentForm(CommentForm):    
-    email         = forms.EmailField(label=_("Email address") , required=False)
+    email   = forms.EmailField(label=_("Email address") , required=False)
+    name          = forms.CharField(label=_("Name"), max_length=50 , required=False)
