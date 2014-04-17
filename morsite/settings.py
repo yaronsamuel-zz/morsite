4cc1cd92c629eb35dc25b6c28bf67d3609c36667
yaron samuel <samuel.yaron@gmail.com>
2014-04-17 17:19:35 +0300

    Fixed GA

--- morsite/settings.py
+++ morsite/settings.py
@@ -110,13 +110,20 @@ STATICFILES_FINDERS = (
 SECRET_KEY = 'f3oda#81rs%yu+*-bc%_5@*nmmf0!yiyw23d(!34awfexfc+j-'
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
+if IS_LOCAL:
+    TEMPLATE_LOADERS = ( 
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
-    )),
 #     'django.template.loaders.eggs.Loader',
 )
+else:
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    #     'django.template.loaders.eggs.Loader',
+    )    
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
